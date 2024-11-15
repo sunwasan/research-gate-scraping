c2f0f26ba929eaa5681f7e6c7c212c2ad0a08001
sunwasan <105874427+sunwasan@users.noreply.github.com>
2024-11-16 00:17:17 +0700

    Add logging and directory structure for data management
    - Updated .gitignore to include logs and data directories
    - Implemented logging for better tracking of URL fetching and errors
    - Created data and logs directories if they do not exist
    - Enhanced functions with docstrings for clarity

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from seleniumbase import Driver
 from bs4 import BeautifulSoup
+from datetime import datetime
 from tqdm import tqdm
 import requests
+import logging
 import json 
 import re
 import os
 
+
+destination_dir = 'data'
+log_dir = 'logs'
+os.makedirs(destination_dir, exist_ok=True)
+os.makedirs(log_dir, exist_ok=True)
+    
 def get_research_urls(
     query:str,
     max_results:int=100
@@ -15,13 +23,17 @@ def get_research_urls(
     """
     base_url = "https://www.google.com/search?q="
     all_research_urls = []
+    full_query = f"site:researchgate.net {query}"
+    query = base_url + full_query.replace(" ", "+")
     
     # Calculate how many pages you need to load (each page has 10 results)
     pages_to_load = (max_results // 10)
     
+    logging.info(f"[get_research_urls] Loading {pages_to_load} pages of Google search results\nurl: {query}")
+    
     for page in range(pages_to_load):
         start = page * 10  # Google search pagination (start=0, start=10, start=20, etc.)
-        query_url = f"{base_url}{query}&start={start}"
+        query_url = f"{query}&start={start}"
         
         # Send the GET request to the query URL
         r = requests.get(query_url)
@@ -35,16 +47,19 @@ def get_research_urls(
         # Use regex to find all the ResearchGate URLs
         research_urls = re.findall(r'(https?://www.researchgate.net\S+)', str(hrefs))
         all_research_urls.extend(research_urls)
-        
+        logging.info(f"[get_research_urls] Found {len(research_urls)} ResearchGate URLs on page {page + 1}")
         # Stop if we've gathered enough URLs (up to max_results)
         if len(all_research_urls) >= max_results:
+            logging.info(f"[get_research_urls] Found {len(all_research_urls)} ResearchGate URLs in total")
             break
 
     # Return the filtered URLs (trim to max_results if needed)
     return all_research_urls[:max_results]
 
 def get_page_source(url:str) -> BeautifulSoup:
-
+    """
+    Get the page source of the ResearchGate page
+    """
     driver = Driver(
         browser="chrome",
         uc=True,                   # Enables undetectable mode
@@ -61,11 +76,17 @@ def get_page_source(url:str) -> BeautifulSoup:
     return soup
 
 def extract_abstract(soup: BeautifulSoup) -> dict:
+    """
+    Extract the abstract from the ResearchGate page
+    """
     abstract = soup.find_all('div', attrs={'itemprop':'description'})[0].text
     head = soup.find_all('h1')[0].text
     return {head:abstract}
 
 def save_json(data:dict, filename:str) -> None:
+    """
+    Save the data to a JSON file
+    """
     if os.path.exists(filename):
         with open(filename, 'r') as f:
             old_data = json.load(f)
@@ -77,18 +98,31 @@ def save_json(data:dict, filename:str) -> None:
             json.dump(data, f)
 
 def get_n_save_abstract(url:str, kw:str, max_results:int) -> None:
+    """ 
+    Get the abstract from the ResearchGate page and save it to a JSON file
+    """
+    
+
+    destination_path = os.path.join(destination_dir, f'{kw}_{max_results}_abstracts.json')
+    
     try:
         soup = get_page_source(url)
         abstract = extract_abstract(soup)
-        save_json(abstract, f'data/{kw}_{max_results}_abstracts.json')
+        save_json(abstract, destination_path)
     except Exception as e:
-        print(f"Error: {e}")
-            
+        logging.error(f"[get_n_save_abstract(url = {url}, kw = {kw}, max_results = {max_results}) Error: {e}")
+        pass            
         
 def main(query:str, max_results:int):
     urls = get_research_urls(query, max_results)
     for url in tqdm(urls, desc="Downloading Abstracts" , unit="page", total=len(urls), position=0, leave=True):
-        get_n_save_abstract(url, query, max_results)
-        
+        get_n_save_abstract(url, query, max_results)  
     n_abstracts = len(urls)
-    print(f"Downloaded {n_abstracts} abstracts")
+    logging.info(f"[main] Downloaded {n_abstracts} abstracts for the query: {query}")
+
+if __name__ == "__main__":
+    query = "banana waste"
+    now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    logging.basicConfig(filename=f"logs/{query.replace(" ","_")}_{now}.log", level=logging.INFO)
+    max_results = 100
+    main(query, max_results)
